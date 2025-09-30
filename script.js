54c9673c718cf524e5d095bff3c56eedb6013d12
Aritra Saha <158503009+devritra@users.noreply.github.com>
2025-09-30 22:41:37 +0530

    Arranged the code
    also added code to clear the inputs and close dialog after submitting

--- script.js
+++ script.js
@@ -1,48 +1,55 @@
 const dialog = document.querySelector(".control_box dialog");
 const showDialogBtn = document.querySelector(".control_box>button");
-
-showDialogBtn.addEventListener("click",()=>{
-    dialog.showModal();
-})
-//array
-const bookShelf = [];
-//submit button
-const submitBtn = document.querySelector(".form_cells:last-of-type>button");
-//inputs
 const bookName = document.querySelector("#book_name");
 const authorName = document.querySelector("#author_name");
-let pageNo = document.querySelector("#page_no");
+const pageNo = document.querySelector("#page_no");
 const bookImage = document.querySelector("#book_image");
-//book display area
+const submitBtn = document.querySelector(".form_cells:last-of-type>button");
 const bookDisplayArea = document.querySelector(".book_display");
-//new elements
+const bookShelf = [];
+ 
+showDialogBtn.addEventListener("click",()=> // "New Book" button eventlistner
+{
+    dialog.showModal();
+})
 
-
-
-function Book(name, author, pages, image){
+function Book(name, author, pages, image) // book constructor
+{
     this.name = name;
     this.author = author;
     this.pages = pages;
     this.image = image;
     this.id = crypto.randomUUID();
 }
-function addBookToBookShelf(name, author, pages, image){
-    if(image===""){
+function addBookToBookShelf(name, author, pages, image) // makes the book and adds to the bookshelf array
+{ 
+    if(image==="")
+    {
         image = "./img/3854108_81501.jpg"
         const book = new Book(name, author, pages, image);
         bookShelf.push(book);
         return;
-    } else {
+    } 
+    else 
+    {
         const book = new Book(name, author, pages, image);
         bookShelf.push(book);
         return;
     }
 }
-function displayBookCards(bookShelf){
+function toggleReadBtn(button) // toggles the state of Read button
+{
+    button.textContent = button.textContent === "Not Read"?"Read":"Not Read";
+    button.style.backgroundColor = button.style.backgroundColor === "rgb(255, 102, 102)"? "rgb(123, 220, 123)":"rgb(255, 102, 102)";
+    button.style.color = button.style.color === "rgb(136, 0, 0)"? "rgb(0, 104, 0)":"rgb(136, 0, 0)";
+    return;
+}
+function displayBookCards(bookShelf) // displays the book object information in respective cards on the page
+{
     bookDisplayArea.innerHTML = "";
     let shelfLength = bookShelf.length - 1;
-    for(let i = 0; i <= shelfLength; i++){
-        //aaaaaaa
+    for(let i = 0; i <= shelfLength; i++)
+    {
         const bookCard = document.createElement("div");
         const bookImageHolder = document.createElement("img");
         const bookNameHolder = document.createElement("h3");
@@ -51,21 +58,21 @@ function displayBookCards(bookShelf){
         const bookCardBtnHolder = document.createElement("div");
         const readBtn = document.createElement("button");
         const bookRemoveBtn = document.createElement("button");
-
         bookCard.classList.add("book_card");
         bookCardBtnHolder.classList.add("book_card_btn_holder")
         readBtn.textContent = "Not Read";
         readBtn.style.backgroundColor = "rgb(255, 102, 102)";
         readBtn.style.color = "rgb(136, 0, 0)";
-        readBtn.addEventListener("click",(e)=>{
+        readBtn.addEventListener("click",(e)=>
+        {
             toggleReadBtn(e.target);
         })
         bookRemoveBtn.textContent = "Remove";
-        bookRemoveBtn.addEventListener("click",(e)=>{
+        bookRemoveBtn.addEventListener("click",(e)=>
+        {
             e.target.parentNode.parentNode.remove();
             bookShelf.splice(i,1);
         })
-        //aaaaaaa
         let book = bookShelf[i];
         bookNameHolder.textContent = `Name: ${book.name}`;
         authorNameHolder.textContent = `Author: ${book.author}`;
@@ -82,21 +89,20 @@ function displayBookCards(bookShelf){
     }
     return;
 } 
-function toggleReadBtn(button){
-    button.textContent = button.textContent === "Not Read"?"Read":"Not Read";
-    button.style.backgroundColor = button.style.backgroundColor === "rgb(255, 102, 102)"? "rgb(123, 220, 123)":"rgb(255, 102, 102)";
-    button.style.color = button.style.color === "rgb(136, 0, 0)"? "rgb(0, 104, 0)":"rgb(136, 0, 0)";
-    return;
-}
-// submitBtn.addEventListener("click",(e)=>{
-//     e.preventDefault();
-// })
-submitBtn.addEventListener("click", ()=>{
+
+submitBtn.addEventListener("click", ()=> // "Add book" button event lisstner
+{
     let pageNoCheck = Number(pageNo.value);
-    if(bookName.value==="" || authorName.value==="" || pageNoCheck === 0 || isNaN(pageNoCheck)){
+    if(bookName.value==="" || authorName.value==="" || pageNoCheck === 0 || isNaN(pageNoCheck))
+    {
         return;
     }
     addBookToBookShelf(bookName.value, authorName.value, pageNoCheck,bookImage.value);
+    bookName.value="";
+    authorName.value="";
+    pageNo.value="";
+    bookImage.value="";
+    dialog.close();
     displayBookCards(bookShelf);
     return;
 })
